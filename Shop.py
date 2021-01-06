9203962e582da8706cf32ee0ae42824bb7a17a11
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2021-01-06 17:19:35 +0400

    Update Shop.py

--- Shop.py
+++ Shop.py
@@ -1,7 +1,6 @@
-import random
 import pygame
 
-from main import load_image
+from Main import load_image
 
 
 class Shop(pygame.sprite.Sprite):
@@ -13,12 +12,13 @@ class Shop(pygame.sprite.Sprite):
         self.image = Shop.image_shop
         self.image_rofl = Shop.image_rofl
         self.rect = self.image.get_rect()
-        self.rect.x = 770
-        self.rect.y = 10
+        self.rect.x = 600
+        self.rect.y = 100
 
     def update(self, *args):
-        self.rect = self.rect.move(random.randrange(3) - 1,
-                                   random.randrange(3) - 1)
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
                 self.rect.collidepoint(args[0].pos):
             self.image = self.image_rofl
+
+            # СОЗДАТЬ ФУНКЦИЮ КОТОРАЯ ОТКРЫВАЕТ НОВОЕ ОКНО И НЕ ЗАКРЫВАЕТ
+            # СТАРОЕ И В КОТОРОМ ТИПО МОЖНО БУДЕТ КУПИТЬ ЧТО ТО
