f7d270c60e64536fc5b96dfeaef7f536386cc421
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2021-01-06 17:21:34 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,37 @@
+import os
+import sys
 import pygame
-from Board import Board
-from MainMenu import Menu
 
-WINDOW_SIZE = 600, 600
+import MainMenu
+
+CELL_COUNTRY = (5, 20)  # ячейка деревни
+PATH = 'images'
+WINDOW_SIZE = (600, 600)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(
+        PATH, name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
 
 if __name__ == '__main__':
+    # инициализация Pygame:
     pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-    main_menu = Menu()
+    # размеры окна:
+    width, height = WINDOW_SIZE
+    screen = pygame.display.set_mode([width, height])
     running = True
+    main_menu = MainMenu.Menu()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
             screen.fill((0, 0, 0))
             main_menu.render(screen, event)
