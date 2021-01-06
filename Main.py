a5fb7885a840a45c414fac3d669e3d317cb21eb3
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2021-01-06 16:29:42 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pygame
 from Board import Board
 from MainMenu import Menu
+import shop
 
 WINDOW_SIZE = 600, 600
 
@@ -9,11 +10,39 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(WINDOW_SIZE)
     main_menu = Menu()
     running = True
+    
+    #часть кода Андрея
+    board = Board(25, 25)
+    screen.fill((0, 0, 0))
+    board.render()
+    
+    # создадим группу, содержащую все спрайты
+    all_sprites = pygame.sprite.Group()
+    # получим картинку(surface)
+    shop_image = load_image("shop.png")
+    # добавляем спрайты
+    for _ in range(1):
+        shop.Shop(all_sprites)
+    # рисуем все спрайты на screen
+    all_sprites.draw(screen)
+
+    pygame.display.flip()
 
     while running:
+        
+        screen.fill((0, 0, 0))
+        board.render()
+
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
+                all_sprites.update(event)
 
-            screen.fill((0, 0, 0))
             main_menu.render(screen, event)
